deblasis/ghostty

fix(os/path): honor PATHEXT when resolving bare command names on Windows (#296)
* fix(os/path): honor PATHEXT when resolving bare command names on Windows

On Windows, the path resolver now honors the PATHEXT environment variable
when searching for bare command names (names without an extension). This
fixes the issue where 'pwsh' fails to resolve to 'pwsh.exe' and similar
problems with other common commands.

The implementation:
- Reads PATHEXT at function entry (fallback to .COM;.EXE;.BAT;.CMD if unset)
- Parses it into a list of extensions (semicolon-separated)
- For bare names, tries the literal name first, then appends each PATHEXT
  extension in order
- For names with extensions, tries literally only (preserves current behavior)
- POSIX branch unchanged

Edge cases covered:
- Bare 'pwsh' resolves to first 'pwsh.<PATHEXT>' found
- 'pwsh.exe' still resolves literally
- 'foo.bar' (unknown extension) tries literal only, NOT foo.bar.exe
- Empty/malformed PATHEXT falls back to default list
- Very long concatenations still guarded by PathTooLong

Closes # 294

* fix: restore AccessDenied accumulation in path.expand

The refactored code should preserve the original behavior of accumulating
AccessDenied errors and returning them if no executable is found. This
allows proper error reporting when files are found but not accessible.

src/os/path.zig
@@ -6,6 +6,10 @@ const testing = std.testing;
 /// Search for "cmd" in the PATH and return the absolute path. This will
 /// always allocate if there is a non-null result. The caller must free the
 /// resulting value.
+///
+/// On Windows, honors PATHEXT when searching for bare command names.
+/// If cmd is already a path or has an extension, tries it literally first
+/// before attempting PATHEXT extensions.
 pub fn expand(alloc: Allocator, cmd: []const u8) !?[]u8 {
     // If the command already contains a path separator, return as-is.
     // POSIX: '/'. Windows additionally accepts '\\' and drive-letter
@@ -30,43 +34,108 @@ pub fn expand(alloc: Allocator, cmd: []const u8) !?[]u8 {
     };
     defer if (builtin.os.tag == .windows) alloc.free(PATH);
 
+    // Parse PATHEXT on Windows
+    var pathext_list: ?[][]const u8 = null;
+    var pathext_buf: ?[]u8 = null;
+    const has_extension = std.mem.indexOfScalar(u8, cmd, '.') != null;
+    defer {
+        if (pathext_buf) |pb| alloc.free(pb);
+        if (pathext_list) |pl| alloc.free(pl);
+    }
+
+    if (builtin.os.tag == .windows and !has_extension) {
+        const pathext_str = blk: {
+            if (std.process.getenvW(std.unicode.utf8ToUtf16LeStringLiteral("PATHEXT"))) |we| {
+                const utf8_pathext = try std.unicode.utf16LeToUtf8Alloc(alloc, we);
+                break :blk utf8_pathext;
+            } else {
+                // Fallback to default Windows extensions
+                break :blk try alloc.dupe(u8, ".COM;.EXE;.BAT;.CMD");
+            }
+        };
+        pathext_buf = pathext_str;
+
+        // Count semicolons to determine how many extensions
+        var ext_count: usize = 1;
+        for (pathext_str) |ch| {
+            if (ch == ';') ext_count += 1;
+        }
+
+        // Allocate and populate extension list
+        pathext_list = try alloc.alloc([]const u8, ext_count);
+        var idx: usize = 0;
+        var it = std.mem.tokenizeScalar(u8, pathext_str, ';');
+        while (it.next()) |ext| {
+            pathext_list.?[idx] = ext;
+            idx += 1;
+        }
+    }
+
     var path_buf: [std.fs.max_path_bytes]u8 = undefined;
     var it = std.mem.tokenizeScalar(u8, PATH, std.fs.path.delimiter);
     var seen_eacces = false;
     while (it.next()) |search_path| {
-        // We need enough space in our path buffer to store this
-        const path_len = search_path.len + cmd.len + 1;
-        if (path_buf.len < path_len) return error.PathTooLong;
+        // First, try the command as-is (literal match, or if it has an extension)
+        if (try tryPathImpl(alloc, search_path, cmd, &path_buf, &seen_eacces)) |result| {
+            return result;
+        }
 
-        // Copy in the full path
-        @memcpy(path_buf[0..search_path.len], search_path);
-        path_buf[search_path.len] = std.fs.path.sep;
-        @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
-        path_buf[path_len] = 0;
-        const full_path = path_buf[0..path_len :0];
+        // On Windows, if no extension, try with PATHEXT extensions
+        if (builtin.os.tag == .windows and !has_extension and pathext_list != null) {
+            for (pathext_list.?) |ext| {
+                // Build cmd + extension
+                const combined_len = cmd.len + ext.len;
+                if (combined_len > std.fs.max_path_bytes) return error.PathTooLong;
+                var cmd_with_ext: [std.fs.max_path_bytes]u8 = undefined;
+                @memcpy(cmd_with_ext[0..cmd.len], cmd);
+                @memcpy(cmd_with_ext[cmd.len..][0..ext.len], ext);
+                const cmd_ext_str = cmd_with_ext[0..combined_len];
 
-        // Stat it
-        const f = std.fs.cwd().openFile(
-            full_path,
-            .{},
-        ) catch |err| switch (err) {
-            error.FileNotFound => continue,
-            error.AccessDenied => {
-                // Accumulate this and return it later so we can try other
-                // paths that we have access to.
-                seen_eacces = true;
-                continue;
-            },
-            else => return err,
-        };
-        defer f.close();
-        const stat = try f.stat();
-        if (stat.kind != .directory and isExecutable(stat.mode)) {
-            return try alloc.dupe(u8, full_path);
+                if (try tryPathImpl(alloc, search_path, cmd_ext_str, &path_buf, &seen_eacces)) |result| {
+                    return result;
+                }
+            }
         }
     }
 
     if (seen_eacces) return error.AccessDenied;
+
+    return null;
+}
+
+/// Helper function to try opening a file at search_path/cmd.
+/// Returns the allocated full path on success, null on FileNotFound,
+/// tracks AccessDenied in seen_eacces pointer, or error on other failures.
+fn tryPathImpl(alloc: Allocator, search_path: []const u8, cmd: []const u8, path_buf: *[std.fs.max_path_bytes]u8, seen_eacces: *bool) !?[]u8 {
+    const path_len = search_path.len + cmd.len + 1;
+    if (path_buf.len < path_len) return error.PathTooLong;
+
+    // Copy in the full path
+    @memcpy(path_buf[0..search_path.len], search_path);
+    path_buf[search_path.len] = std.fs.path.sep;
+    @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
+    path_buf[path_len] = 0;
+    const full_path = path_buf[0..path_len :0];
+
+    // Try to open the file
+    const f = std.fs.cwd().openFile(
+        full_path,
+        .{},
+    ) catch |err| switch (err) {
+        error.FileNotFound => return null,
+        error.AccessDenied => {
+            // Accumulate this and return it later so we can try other
+            // paths that we have access to.
+            seen_eacces.* = true;
+            return null;
+        },
+        else => return err,
+    };
+    defer f.close();
+    const stat = try f.stat();
+    if (stat.kind != .directory and isExecutable(stat.mode)) {
+        return try alloc.dupe(u8, full_path);
+    }
 
     return null;
 }
@@ -121,4 +190,33 @@ test "expand: windows bare cmd.exe resolves on PATH" {
     // System32\cmd.exe lives on the default Windows PATH.
     try testing.expect(std.ascii.endsWithIgnoreCase(path, "cmd.exe"));
     try testing.expect(path.len > "cmd.exe".len);
+}
+
+test "expand: windows bare pwsh resolves via PATHEXT" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    // This test requires pwsh.exe to be on the system PATH.
+    // If not found, it returns null rather than erroring.
+    const path = try expand(testing.allocator, "pwsh");
+    if (path) |p| {
+        defer testing.allocator.free(p);
+        try testing.expect(std.ascii.endsWithIgnoreCase(p, "pwsh.exe") or std.ascii.endsWithIgnoreCase(p, "pwsh.com") or std.ascii.endsWithIgnoreCase(p, "pwsh.bat") or std.ascii.endsWithIgnoreCase(p, "pwsh.cmd"));
+        try testing.expect(p.len > "pwsh".len);
+    }
+}
+
+test "expand: windows name with extension does not PATHEXT hunt" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    // If we provide a name with an extension (even unknown), it should
+    // try that literally, not attempt PATHEXT extensions.
+    const path = try expand(testing.allocator, "cmd.xyz");
+    try testing.expect(path == null);
+}
+
+test "expand: windows extension present bypasses PATHEXT" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    // cmd.exe should resolve literally via the literal-first attempt,
+    // not via PATHEXT hunting.
+    const path = (try expand(testing.allocator, "cmd.exe")).?;
+    defer testing.allocator.free(path);
+    try testing.expect(std.ascii.endsWithIgnoreCase(path, "cmd.exe"));
 }

deblasis/wintty

src/os/path.zig
@@ -6,58 +6,127 @@ const testing = std.testing;
 /// Search for "cmd" in the PATH and return the absolute path. This will
 /// always allocate if there is a non-null result. The caller must free the
 /// resulting value.
+///
+/// On Windows, honors PATHEXT when searching for bare command names.
+/// If cmd is already a path or has an extension, tries it literally first
+/// before attempting PATHEXT extensions.
 pub fn expand(
     io: std.Io,
     alloc: Allocator,
     environ_map: *const std.process.Environ.Map,
     cmd: []const u8,
 ) !?[]u8 {
-    // If the command already contains a slash, then we return it as-is
-    // because it is assumed to be absolute or relative.
-    if (std.mem.indexOfScalar(u8, cmd, '/') != null) {
+    // If the command already contains a path separator, return as-is.
+    // POSIX: '/'. Windows additionally accepts '\\' and drive-letter
+    // prefixes like 'X:'. Without the Windows extensions a path such
+    // as `C:\Windows\System32\cmd.exe` would miss the fast path and
+    // get joined onto every PATH dir, always producing null.
+    const already_path = std.mem.indexOfScalar(u8, cmd, '/') != null or
+        (builtin.os.tag == .windows and
+            (std.mem.indexOfScalar(u8, cmd, '\\') != null or
+                (cmd.len >= 2 and std.ascii.isAlphabetic(cmd[0]) and cmd[1] == ':')));
+    if (already_path) {
         return try alloc.dupe(u8, cmd);
     }
 
     const PATH = environ_map.get("PATH") orelse return null;
 
+    // Parse PATHEXT on Windows. Only bare names (no extension) need it;
+    // anything already carrying an extension is tried literally.
+    var pathext_list: ?[][]const u8 = null;
+    const has_extension = std.mem.indexOfScalar(u8, cmd, '.') != null;
+    defer if (pathext_list) |pl| alloc.free(pl);
+
+    if (builtin.os.tag == .windows and !has_extension) {
+        // Fall back to the documented default set when PATHEXT is unset.
+        const pathext_str = environ_map.get("PATHEXT") orelse ".COM;.EXE;.BAT;.CMD";
+
+        var ext_count: usize = 0;
+        var count_it = std.mem.tokenizeScalar(u8, pathext_str, ';');
+        while (count_it.next()) |_| ext_count += 1;
+
+        pathext_list = try alloc.alloc([]const u8, ext_count);
+        var idx: usize = 0;
+        var ext_it = std.mem.tokenizeScalar(u8, pathext_str, ';');
+        while (ext_it.next()) |ext| {
+            pathext_list.?[idx] = ext;
+            idx += 1;
+        }
+    }
+
     var path_buf: [std.fs.max_path_bytes]u8 = undefined;
     var it = std.mem.tokenizeScalar(u8, PATH, std.fs.path.delimiter);
     var seen_eacces = false;
     while (it.next()) |search_path| {
-        // We need enough space in our path buffer to store this
-        const path_len = search_path.len + cmd.len + 1;
-        if (path_buf.len < path_len) return error.PathTooLong;
+        // First, try the command as-is (literal match, or if it has an
+        // extension).
+        if (try tryPathImpl(io, alloc, search_path, cmd, &path_buf, &seen_eacces)) |result| {
+            return result;
+        }
 
-        // Copy in the full path
-        @memcpy(path_buf[0..search_path.len], search_path);
-        path_buf[search_path.len] = std.fs.path.sep;
-        @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
-        path_buf[path_len] = 0;
-        const full_path = path_buf[0..path_len :0];
+        // On Windows, if no extension, try with PATHEXT extensions.
+        if (builtin.os.tag == .windows and !has_extension and pathext_list != null) {
+            for (pathext_list.?) |ext| {
+                const combined_len = cmd.len + ext.len;
+                if (combined_len > std.fs.max_path_bytes) return error.PathTooLong;
+                var cmd_with_ext: [std.fs.max_path_bytes]u8 = undefined;
+                @memcpy(cmd_with_ext[0..cmd.len], cmd);
+                @memcpy(cmd_with_ext[cmd.len..][0..ext.len], ext);
+                const cmd_ext_str = cmd_with_ext[0..combined_len];
 
-        // Stat it
-        const f = std.Io.Dir.cwd().openFile(
-            io,
-            full_path,
-            .{},
-        ) catch |err| switch (err) {
-            error.FileNotFound => continue,
-            error.AccessDenied => {
-                // Accumulate this and return it later so we can try other
-                // paths that we have access to.
-                seen_eacces = true;
-                continue;
-            },
-            else => return err,
-        };
-        defer f.close(io);
-        const stat = try f.stat(io);
-        if (stat.kind != .directory and isExecutable(stat.permissions)) {
-            return try alloc.dupe(u8, full_path);
+                if (try tryPathImpl(io, alloc, search_path, cmd_ext_str, &path_buf, &seen_eacces)) |result| {
+                    return result;
+                }
+            }
         }
     }
 
     if (seen_eacces) return error.AccessDenied;
+
+    return null;
+}
+
+/// Helper function to try opening a file at search_path/cmd.
+/// Returns the allocated full path on success, null on FileNotFound,
+/// tracks AccessDenied in seen_eacces pointer, or error on other failures.
+fn tryPathImpl(
+    io: std.Io,
+    alloc: Allocator,
+    search_path: []const u8,
+    cmd: []const u8,
+    path_buf: *[std.fs.max_path_bytes]u8,
+    seen_eacces: *bool,
+) !?[]u8 {
+    const path_len = search_path.len + cmd.len + 1;
+    if (path_buf.len < path_len) return error.PathTooLong;
+
+    // Copy in the full path
+    @memcpy(path_buf[0..search_path.len], search_path);
+    path_buf[search_path.len] = std.fs.path.sep;
+    @memcpy(path_buf[search_path.len + 1 ..][0..cmd.len], cmd);
+    path_buf[path_len] = 0;
+    const full_path = path_buf[0..path_len :0];
+
+    // Stat it
+    const f = std.Io.Dir.cwd().openFile(
+        io,
+        full_path,
+        .{},
+    ) catch |err| switch (err) {
+        error.FileNotFound => return null,
+        error.AccessDenied => {
+            // Accumulate this and return it later so we can try other
+            // paths that we have access to.
+            seen_eacces.* = true;
+            return null;
+        },
+        else => return err,
+    };
+    defer f.close(io);
+    const stat = try f.stat(io);
+    if (stat.kind != .directory and isExecutable(stat.permissions)) {
+        return try alloc.dupe(u8, full_path);
+    }
 
     return null;
 }
@@ -97,4 +166,73 @@ test "expand: slash" {
     const path = (try expand(testing.io, testing.allocator, &environ_map, "foo/env")).?;
     defer testing.allocator.free(path);
     try testing.expect(path.len == 7);
+}
+
+test "expand: windows backslash passes through" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    const input = "C:\\Windows\\System32\\cmd.exe";
+    const path = (try expand(testing.io, testing.allocator, &environ_map, input)).?;
+    defer testing.allocator.free(path);
+    try testing.expectEqualStrings(input, path);
+}
+
+test "expand: windows drive-letter only passes through" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    // No separator, but the drive prefix means this is already a
+    // path (drive-relative) and expand() must not treat it as a
+    // bare name to search PATH for.
+    const input = "C:cmd.exe";
+    const path = (try expand(testing.io, testing.allocator, &environ_map, input)).?;
+    defer testing.allocator.free(path);
+    try testing.expectEqualStrings(input, path);
+}
+
+test "expand: windows bare cmd.exe resolves on PATH" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    const path = (try expand(testing.io, testing.allocator, &environ_map, "cmd.exe")).?;
+    defer testing.allocator.free(path);
+    // System32\cmd.exe lives on the default Windows PATH.
+    try testing.expect(std.ascii.endsWithIgnoreCase(path, "cmd.exe"));
+    try testing.expect(path.len > "cmd.exe".len);
+}
+
+test "expand: windows bare pwsh resolves via PATHEXT" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    // This test requires pwsh.exe to be on the system PATH.
+    // If not found, it returns null rather than erroring.
+    const path = try expand(testing.io, testing.allocator, &environ_map, "pwsh");
+    if (path) |p| {
+        defer testing.allocator.free(p);
+        try testing.expect(std.ascii.endsWithIgnoreCase(p, "pwsh.exe") or std.ascii.endsWithIgnoreCase(p, "pwsh.com") or std.ascii.endsWithIgnoreCase(p, "pwsh.bat") or std.ascii.endsWithIgnoreCase(p, "pwsh.cmd"));
+        try testing.expect(p.len > "pwsh".len);
+    }
+}
+
+test "expand: windows name with extension does not PATHEXT hunt" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    // If we provide a name with an extension (even unknown), it should
+    // try that literally, not attempt PATHEXT extensions.
+    const path = try expand(testing.io, testing.allocator, &environ_map, "cmd.xyz");
+    try testing.expect(path == null);
+}
+
+test "expand: windows extension present bypasses PATHEXT" {
+    if (builtin.os.tag != .windows) return error.SkipZigTest;
+    var environ_map = try testing.environ.createMap(testing.allocator);
+    defer environ_map.deinit();
+    // cmd.exe should resolve literally via the literal-first attempt,
+    // not via PATHEXT hunting.
+    const path = (try expand(testing.io, testing.allocator, &environ_map, "cmd.exe")).?;
+    defer testing.allocator.free(path);
+    try testing.expect(std.ascii.endsWithIgnoreCase(path, "cmd.exe"));
 }